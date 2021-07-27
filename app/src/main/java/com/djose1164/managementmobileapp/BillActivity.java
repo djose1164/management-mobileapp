6bd9313c46a550862a089fb02904437b093bbfd1
Jose S. Daniel <djose1164@gmail.com>
2021-07-27 01:22:38 -0400

    Bill activity created

---
+++ app/src/main/java/com/djose1164/managementmobileapp/BillActivity.java
@@ -0,0 +1,14 @@
+package com.djose1164.managementmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill);
+    }
+}
--- app/src/main/java/com/djose1164/managementmobileapp/MainActivity.java
+++ app/src/main/java/com/djose1164/managementmobileapp/MainActivity.java
@@ -1,31 +1,30 @@
 package com.djose1164.managementmobileapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.app.Activity;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textView;
-    private ToastMsg toastMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        this.textView = findViewById(R.id.textView);
     }
-
-    public void btn_newBill(View view) {
-        Toast toast = Toast.makeText(this, "Holaa", Toast.LENGTH_LONG);
+    private void toastMsg(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
         toast.show();
+    }
+    public void btn_newBill(View view) {
+        Intent intent = new Intent(this, BillActivity.class);
+        startActivity(intent);
     }
 
     public void btn_showBills(View view) {
-        toastMsg.showMsg(this, "Mostrando facturas");
+        this.toastMsg("Viendo facturas");
     }
 }
